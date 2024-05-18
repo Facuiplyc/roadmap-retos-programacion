32bfb8ab21dab2a609211697a3cfa3e02a9f03eb
Facuiplyc <srxiplyc@gmail.com>
2024-05-17 21:23:09 -0300

    Add files via upload

---
+++ #00- Python.py
@@ -0,0 +1,46 @@
+#https://www.python.org/
+
+# ¿Cómo poner comentarios en Python?
+# Puedes poner un comentario en Python utilizando el símbolo de numeral (#) al inicio de cada una de las líneas que quieres comentar o con tres comillas dobles (""") 
+# al inicio y al final del bloque de código aludido
+
+"""
+De esta forma tambien podemos comentar
+y incluso en mas de una linea
+
+"""
+
+# Variable 
+valor = 52
+# Constante 
+IVA = 21
+
+# Tipos de variables numericas
+
+# entero
+
+cantidad = 5
+
+# float
+
+resto = 2.5
+
+# Tipos de variables de texto
+
+# Character o String
+
+nombre = "Facundo"
+
+apellido = 'Meridda'
+
+lugar_de_nacimiento = "Argentina"
+
+cadena_de_texto = "Hola como estas"
+
+# Tipos de variables Booleanas (True or False)
+
+soy_ingeniero = False
+
+soy_estudiante = True
+
+print("Hola Python!")
